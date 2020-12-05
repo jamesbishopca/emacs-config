9d1c1fcae0f485a65083badfab1f897c2c3ec99c
James Bishop <james@jamesbishop.ca>
2020-12-04 22:53:00 -0400

    loaded setup-emacs-lisp

--- init.el
+++ init.el
@@ -32,24 +32,9 @@
 ;; add customizations folder to load path, then load files
 (add-to-list 'load-path "~/.emacs.d/customizations")
 (load "editing")
+(load "setup-emacs-lisp")
 (load "setup-common-lisp")
 (load "ui")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("c4cecd97a6b30d129971302fd8298c2ff56189db0a94570e7238bc95f9389cfb" default)))
- '(package-selected-packages (quote (slime))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 (provide 'init)
 ;;; init.el ends here
